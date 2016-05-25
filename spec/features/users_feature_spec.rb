082037a9c04d60bd20f2cbf8ac4325a0f682ce62
emilio <emsmnndz@gmail.com>
2016-05-25 16:13:03 +0100

    users can sign in and sign out

---
+++ spec/features/users_feature_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+feature 'Users can sign in and sing out' do
+  context 'user not signed at the homepage' do
+    it "should see a 'sign in' link and a 'sign up' link" do
+      visit '/'
+      expect(page).to have_link 'Sign in'
+      expect(page).to have_link 'Sign up'
+    end
+    it "should not see a 'sign out' link" do
+      visit'/'
+      expect(page).not_to have_link 'Sign out'
+    end
+  end
+  context 'user signed in on the homepage' do
+    it "should see a 'sign out' link" do
+      visit '/'
+      click_link 'Sign up'
+      fill_in 'Email', with: 'test@example.com'
+      fill_in 'Password', with: 'testtest'
+      fill_in 'Password confirmation', with: 'testtest'
+      click_button 'Sign up'
+      visit '/'
+      expect(page).not_to have_link 'Sign in'
+      expect(page).not_to have_link 'Sign up'
+    end
+  end
+end
